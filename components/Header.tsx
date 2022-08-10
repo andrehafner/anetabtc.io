1e3eb3d3490a10c61f1fdfbabfaffd59aedc5b76
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-08-10 20:58:37 +0800

    add metadata to html file

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      <img src="/logo-theme-dark.png"></img>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+
 export default function Home() {
   return (
     <div className="theme-dark next-container">
-      hello      
+      <Head>
+        <link rel="icon" href="/favicon.ico"></link>
+        <title>anetBTC Staking Platform</title>
+      </Head>
+      <Header></Header>
     </div>
   )
 }
