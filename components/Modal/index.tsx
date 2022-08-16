50b129c5d6bef7ae3e7b4786b0d511e7a3abb96f
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-08-16 22:31:59 +0800

    add close button for modal

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -12,8 +12,9 @@ const Modal = ({
   return (
     <div className="fixed w-screen h-screen top-0 left-0 flex items-center justify-center z-10">
       <div className="fixed bg-black opacity-80 w-full h-full"></div>
-      <div className="p-5 rounded-2xl z-20 component min-w-80" ref={ref}>
+      <div className="p-5 rounded-2xl z-20 component min-w-80 flex flex-col gap-4 items-center" ref={ref}>
         {children}
+        <button className="clickable button rounded-lg py-1 px-2.5 w-fit" onClick={closeModal}>Close</button>
       </div>
     </div>
   );
