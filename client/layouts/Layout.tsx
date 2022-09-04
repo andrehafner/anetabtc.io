fc01ff61a7b5b36e6777589d9caaa1bffee2497d
aryelciu001 <ryelcius.alfredo29@gmail.com>
2022-09-04 23:14:18 +0800

    chore/web: add max width for container

--- client/layouts/Layout.tsx
+++ client/layouts/Layout.tsx
@@ -18,13 +18,17 @@ const Layout = ({ children }: { children: JSX.Element }) => {
   }, []);
 
   return (
-    <div className={`theme-${theme} next-container relative min-h-full w-full`}>
-      <ErrorModal></ErrorModal>
-      <Head>
-        <link rel="icon" href="/favicon.ico"></link>
-        <title>anetaBTC Staking Platform</title>
-      </Head>
-      {children}
+    <div
+      className={`theme-${theme} next-container relative min-h-full w-full flex flex-col items-center`}
+    >
+      <div className="w-full max-w-6xl">
+        <ErrorModal></ErrorModal>
+        <Head>
+          <link rel="icon" href="/favicon.ico"></link>
+          <title>anetaBTC Staking Platform</title>
+        </Head>
+        {children}
+      </div>
     </div>
   );
 };
